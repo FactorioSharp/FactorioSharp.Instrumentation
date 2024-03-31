c4f319799cfe314fead642de49905a9acb0a7384
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-03-31 17:28:56 +0200

    extract client management to separate class

--- FactorioSharp.Instrumentation/Scheduling/FactorioInstrumentationBackgroundWorker.cs
+++ FactorioSharp.Instrumentation/Scheduling/FactorioInstrumentationBackgroundWorker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.Metrics;
+using FactorioSharp.Instrumentation.Integration;
 using FactorioSharp.Instrumentation.Integration.Jobs;
 using FactorioSharp.Instrumentation.Meters;
 using FactorioSharp.Instrumentation.Model;
@@ -17,21 +18,17 @@ class FactorioInstrumentationBackgroundWorker : BackgroundService
     public static readonly string MeterName = typeof(FactorioInstruments).Assembly.GetName().Name!;
     public static readonly string MeterVersion = typeof(FactorioInstruments).Assembly.GetName().Version!.ToString();
 
-    readonly string _host;
-    readonly int _port;
-    readonly string _password;
+    readonly FactorioRconClientProvider _clientProvider;
+    bool _isConnected;
     readonly FactorioMeterOptionsInternal _options;
     readonly ILogger<FactorioInstrumentationBackgroundWorker> _logger;
-    FactorioRconClient? _client;
     readonly FactorioServerData _cache;
     readonly JobCollection _jobs;
     Meter? _meter;
 
     public FactorioInstrumentationBackgroundWorker(string host, int port, string password, IOptions<FactorioMeterOptions> options, ILoggerFactory loggerFactory)
     {
-        _host = host;
-        _port = port;
-        _password = password;
+        _clientProvider = new FactorioRconClientProvider(host, port, password, loggerFactory.CreateLogger<FactorioRconClientProvider>());
         _options = new FactorioMeterOptionsInternal(options.Value);
         _logger = loggerFactory.CreateLogger<FactorioInstrumentationBackgroundWorker>();
 
@@ -57,7 +54,7 @@ class FactorioInstrumentationBackgroundWorker : BackgroundService
     public override Task StopAsync(CancellationToken cancellationToken)
     {
         _logger.LogInformation("Stopping background worker...");
-        _client?.Dispose();
+        _clientProvider.Dispose();
         _meter?.Dispose();
         return base.StopAsync(cancellationToken);
     }
@@ -69,25 +66,16 @@ class FactorioInstrumentationBackgroundWorker : BackgroundService
 
         await _jobs.ExecuteOnStartAsync(_cache, _options, stoppingToken);
 
-        bool isConnected = false;
+        _isConnected = false;
 
         while (!stoppingToken.IsCancellationRequested)
         {
             DateTime startTime = DateTime.Now;
 
-            GetConnectedClientResult getClientResult = await TryGetConnectedClient();
-            if (getClientResult.Succeeded)
+            FactorioRconClientProvider.GetConnectedClientResult result = await _clientProvider.TryGetConnectedClient();
+            if (result.Succeeded)
             {
-                if (!isConnected)
-                {
-                    isConnected = true;
-                    await _jobs.ExecuteOnConnectAsync(_cache, getClientResult.Client!, _options, stoppingToken);
-
-                    _meter ??= CreateMeter();
-                    FactorioInstruments.Setup(_meter, _cache, _options);
-                }
-
-                await _jobs.ExecuteOnTickAsync(_cache, getClientResult.Client!, _options, stoppingToken);
+                await HandleTick(result.Client!, stoppingToken);
 
                 TimeSpan elapsed = DateTime.Now - startTime;
                 TimeSpan toWait = minDelayBetweenObservations - elapsed;
@@ -99,17 +87,7 @@ class FactorioInstrumentationBackgroundWorker : BackgroundService
             }
             else
             {
-                if (isConnected)
-                {
-                    isConnected = false;
-                    await _jobs.ExecuteOnDisconnectAsync(_cache, _options, stoppingToken);
-
-                    _meter?.Dispose();
-                    _meter = CreateMeter();
-                }
-
-                _logger.LogError(getClientResult.Exception, "Could not connect to server at {host}:{port}. Reason: {reason}.", _host, _port, getClientResult.FailureReason);
-
+                await HandleConnectionFailed(result, stoppingToken);
                 await Task.Delay(minDelayBetweenConnectionAttempts, stoppingToken);
             }
         }
@@ -117,70 +95,39 @@ class FactorioInstrumentationBackgroundWorker : BackgroundService
         await _jobs.ExecuteOnStopAsync(_cache, _options, stoppingToken);
     }
 
+    async Task HandleTick(FactorioRconClient client, CancellationToken stoppingToken)
+    {
+        if (!_isConnected)
+        {
+            _isConnected = true;
+            await _jobs.ExecuteOnConnectAsync(_cache, client, _options, stoppingToken);
+
+            _meter ??= CreateMeter();
+            FactorioInstruments.Setup(_meter, _cache, _options);
+        }
+
+        await _jobs.ExecuteOnTickAsync(_cache, client, _options, stoppingToken);
+    }
+
+    async Task HandleConnectionFailed(FactorioRconClientProvider.GetConnectedClientResult result, CancellationToken stoppingToken)
+    {
+        if (_isConnected)
+        {
+            _isConnected = false;
+            await _jobs.ExecuteOnDisconnectAsync(_cache, _options, stoppingToken);
+
+            _meter?.Dispose();
+            _meter = CreateMeter();
+        }
+
+        _logger.LogError(result.Exception, "Could not connect to server at {host}:{port}. Reason: {reason}.", result.Host, result.Port, result.FailureReason);
+    }
+
     Meter CreateMeter()
     {
         Meter meter = new(MeterName, MeterVersion);
-        meter.CreateObservableGauge("factorio.server.status", () => _client is { Connected: true } ? 1 : 0, "1", "The current status of the factorio server");
+        meter.CreateObservableGauge("factorio.server.status", () => _isConnected ? 1 : 0, "1", "The current status of the factorio server");
 
         return meter;
-    }
-
-    async Task<GetConnectedClientResult> TryGetConnectedClient()
-    {
-        try
-        {
-            if (_client is { Connected: true })
-            {
-                return GetConnectedClientResult.Success(_client);
-            }
-
-            if (_client != null)
-            {
-                _logger.LogDebug("Connection to {host}:{port} has been lost, reconnection attempt...", _host, _port);
-                _client.Dispose();
-            }
-            else
-            {
-                _logger.LogDebug("Connection attempt to {host}:{port}...", _host, _port);
-            }
-
-            _client = new FactorioRconClient(_host, _port);
-
-
-            if (await _client.ConnectAsync(_password))
-            {
-                _logger.LogDebug("Connected to {host}:{port}.", _host, _port);
-                return GetConnectedClientResult.Success(_client);
-            }
-
-            _logger.LogDebug("Connection to {host}:{port} failed.", _host, _port);
-
-            _client.Dispose();
-            _client = null;
-            return GetConnectedClientResult.Failure($"Connection or authentication to {_host}:{_port} failed, double check the host, port and password.");
-        }
-        catch (Exception exn)
-        {
-            return GetConnectedClientResult.Failure(exn);
-        }
-    }
-
-    class GetConnectedClientResult
-    {
-        public bool Succeeded => Client != null;
-        public FactorioRconClient? Client { get; }
-        public string? FailureReason { get; }
-        public Exception? Exception { get; }
-
-        GetConnectedClientResult(FactorioRconClient? client, string? failureReason, Exception? exception)
-        {
-            Client = client;
-            FailureReason = failureReason;
-            Exception = exception;
-        }
-
-        public static GetConnectedClientResult Success(FactorioRconClient client) => new(client, null, null);
-        public static GetConnectedClientResult Failure(Exception exception) => new(null, exception.Message, exception);
-        public static GetConnectedClientResult Failure(string reason) => new(null, reason, null);
     }
 }
