1ee5af99de6aa547fd2dd3e3f791e8a545af48c7
Ismail Bennani <lahkim.bennani.ismail@gmail.com>
2024-04-01 23:21:22 +0200

    add missing call to AddMeter

--- FactorioSharp.Instrumentation.OpenTelemetry/FactorioInstrumentationOpenTelemetryMeterProviderBuilderExtensions.cs
+++ FactorioSharp.Instrumentation.OpenTelemetry/FactorioInstrumentationOpenTelemetryMeterProviderBuilderExtensions.cs
@@ -5,6 +5,12 @@ namespace FactorioSharp.Instrumentation.OpenTelemetry;
 
 public static class FactorioInstrumentationOpenTelemetryMeterProviderBuilderExtensions
 {
-    public static MeterProviderBuilder AddFactorioInstrumentation(this MeterProviderBuilder builder, Action<FactorioInstrumentationOptions>? configureOptions = null) =>
+    public static MeterProviderBuilder AddFactorioInstrumentation(this MeterProviderBuilder builder, Action<FactorioInstrumentationOptions>? configureOptions = null)
+    {
         builder.ConfigureServices(services => services.AddFactorioInstrumentation(configureOptions));
+
+        builder.AddMeter(FactorioInstrumentationConstants.MeterName);
+
+        return builder;
+    }
 }
